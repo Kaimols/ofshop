58a2b35c189db285c527f7c50087b055bdc7824f
vs5jwnpdzn-coder <vs5jwnpdzn@privaterelay.appleid.com>
2026-02-11 04:57:04 +0100

    w

--- gallery.php
+++ gallery.php
@@ -29,7 +29,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lozad.js/1.0.8/lozad.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
     <style>
         * {
